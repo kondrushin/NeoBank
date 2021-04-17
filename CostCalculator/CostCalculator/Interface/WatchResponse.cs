1d5ab1514107612b57ea682714df2095988e3519
kondrushin <kondrushin.a.e@gmail.com>
2021-04-18 00:01:06 +0400

    Add response

---
+++ CostCalculator/CostCalculator/Interface/WatchResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace CostCalculator.Interface
+{
+    public class WatchResponse
+    {
+        [JsonProperty("price")]
+        public int Price { get; set; }
+    }
+}
