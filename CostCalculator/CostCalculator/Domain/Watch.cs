22729d0e145ff0ae7f617d6ace685a6091f05eaa
kondrushin <kondrushin.a.e@gmail.com>
2021-04-17 23:53:35 +0400

    Add watch model

---
+++ CostCalculator/CostCalculator/Domain/Watch.cs
@@ -0,0 +1,32 @@
+﻿namespace CostCalculator.Domain
+{
+    public class Watch
+    {
+        public Watch(
+            string id,
+            string name,
+            int price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public Watch(
+            string id, 
+            string name, 
+            int price, 
+            Discount discount)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Discount = discount;
+        }
+
+        public string Id { get; }
+        public string Name { get; }
+        public int Price { get; }
+        public Discount Discount { get; }
+    }
+}
