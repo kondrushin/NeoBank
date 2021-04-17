9966fde586e6ca0556857e60d15413722a20b60c
kondrushin <kondrushin.a.e@gmail.com>
2021-04-18 00:03:25 +0400

    Register interfaces

---
+++ CostCalculator/CostCalculator/IocRegistrar.cs
@@ -0,0 +1,15 @@
+﻿using CostCalculator.Domain.Services;
+using CostCalculator.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CostCalculator
+{
+    public static class IocRegistrar
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddTransient<ICartFactory, CartFactory>();
+            services.AddTransient<IWatchRepository, WatchRepository>();
+        }
+    }
+}
